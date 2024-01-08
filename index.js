2243e72c8b35ecf4217b090d81023d51aed28548
Adryyy <adrydlg752@gmail.com>
2024-01-09 00:09:01 +0100

    Añadir el color correcto en pantalla

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let colores = ["rgb(255, 89, 94)","rgb(255, 202, 58)","rgb(138, 201, 38)","rgb(202, 103, 2)",
                 "rgb(25, 130, 196)","rgb(106, 76, 147)", "rgb(141, 153, 174)","rgb(201, 173, 167)"]
-function ColorRandom(){
+function RandomColor(){
 
     let random = parseInt(Math.random() * colores.length)
     let color = colores[random]
@@ -11,7 +11,7 @@ function ColorRandom(){
     return color
 }
 
-function AsignarColor(element,callback){      
+function AssingColor(element,callback){      
        let color = callback() // Funcion de numero random
         element.style.background = color// agregar color
 }
@@ -21,13 +21,23 @@ const grid_itemas = document.querySelectorAll('.colorElegible')
 
 // añadir colores elemento a elemento
 grid_itemas.forEach(item =>{
-    AsignarColor(item,ColorRandom)
+    AssingColor(item,RandomColor)
 })
 
 
+//añadir el color
+function CorrectRandomColorOnScreen(random_color){
+    //separar el codigo desde los paréntesis
+    const color = random_color.slice(random_color.indexOf("(")+1,random_color.indexOf(")"));
+
+    // agrega el codigo del color y cambia el estilo de texto a bold
+    const div_rbg = document.querySelector('.rgb')
+    div_rbg.textContent = color
+    div_rbg.style.fontWeight = "bold"
+}
 
 
-
+CorrectRandomColorOnScreen(RandomColor())
 
 
 
