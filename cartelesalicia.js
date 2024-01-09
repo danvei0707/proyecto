f1ddaf2a0e4ae7e7402aa9e8f655e9c5bab33210
Alicia Lavandeira <ali.lav@hotmail.es>
2024-01-09 21:04:48 +0100

    Modificaciones carteles

--- cartelesalicia.js
+++ cartelesalicia.js
@@ -1,27 +1,23 @@
-function GenerarCarteles (aciertos, fallos) {
-    if (aciertos === 3) {
-         ganar();
-    }else if(fallos === 3) {
-         perder();
-    }
- }
- function ganar() {
-    var w = window.open('','','width=100,height=100');
-    var left = (screen.width - w.outerWidth) / 2;
-    var top = (screen.height - w.outerHeight) / 2;
-    w.moveTo(left, top);
-    w.document.write('Ganaste!'); w.document.body.style.backgroundColor = '#008000';
-    w.focus();
-    //w.document.value
-    setTimeout(function() {w.close();}, 3000);
- }
- function perder() {
-    var w = window.open('','','width=100,height=100');
-    var left = (screen.width - w.outerWidth) / 2;
-    var top = (screen.height - w.outerHeight) / 2;
-    w.moveTo(left, top);
-    w.document.write('Perdiste!'); w.document.body.style.backgroundColor = '#990000';
-    w.focus();
-    //w.document.value
-    setTimeout(function() {w.close();}, 3000);
+// function GenerarCarteles (aciertos, fallos) {
+//     if (aciertos === 3) {
+//          ganar();
+//     }else if(fallos === 3) {
+//          perder();
+//     }
+//  }
+ export function ganar() {
+   const rojo= document.querySelector (".win")
+   rojo.classList.remove("oculto");
+  
+    setTimeout(()=> {
+      rojo.classList.add("oculto"); 
+    }, 1500);
+   }
+ export function perder() {
+   const rojo= document.querySelector (".over")
+   rojo.classList.remove("oculto");
+  
+    setTimeout(()=> {
+      rojo.classList.add("oculto"); 
+    }, 1500); 
  }
