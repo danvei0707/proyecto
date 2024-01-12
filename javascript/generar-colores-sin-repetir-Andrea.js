02296fd5fd75065269e23a316573cd4972cfe430
Adryyy <adrydlg752@gmail.com>
2024-01-12 10:31:25 +0100

    Cambio formato de tamaño de titulo y añadir niveles

--- javascript/generar-colores-sin-repetir-Andrea.js
+++ javascript/generar-colores-sin-repetir-Andrea.js
@@ -1,20 +1,54 @@
-function GenerarNumeros() {
-  let r = parseInt(Math.random() * (183 - 72 + 1) + 72);
-  let g = parseInt(Math.random() * (183- 72 + 1) + 72);
-  let b = parseInt(Math.random() * (183 - 72 + 1) + 72);
-  let random;
+const contFallos = document.querySelector('.contFallos p:last-child');
 
+let rangomin = 72;
+let rangomax = 183;
+let nivel = 0
+let num = 9
+
+function Niveles(){
+  console.log("Numeros de niveles");
+  if(nivel === 3){
+    nivel = 0
+  }else if(contFallos.textContent == 2){
+    rangomax += 8
+    rangomin += 8
+    nivel++
+    num--
+
+    console.log(rangomax);
+    console.log(rangomin);
+    console.log(nivel);
+    console.log(num);
+  }
+}
+
+function AplicarColoresInterfaz(r,g,b){
   let body = document.querySelector("body")
   body.style.background = `rgb(${r+43} ,${g+43} ,${b+43})`
-
+  
   let header = document.querySelector("header")
   header.style.background = `rgb(${r - 10} ,${g - 20} ,${b -30})`
 
-  const div_sample = document.querySelector(".enunciado");
-  div_sample.style.boxShadow = `26px 26px 51px rgb(${r - 20} ,${g - 20} ,${b -20}),-26px -26px 51px rgb(${r + 100} ,${g + 100} ,${b +100})`;
+  let carteles = document.querySelector(".puntaje")
+  carteles.style.background = `rgb(${r+4} ,${g+4} ,${b+4} ,0.50)`
 
+  
+  const sombra = document.querySelector(".enunciado");
+  sombra.style.boxShadow = `26px 26px 51px rgb(${r - 20} ,${g - 20} ,${b -20}),-26px -26px 51px rgb(${r + 100} ,${g + 100} ,${b +100})`;
+}
+
+function GenerarNumeros() {
+  Niveles()
+
+  let r = parseInt(Math.random() * (rangomax - rangomin + 1) + rangomin);
+  let g = parseInt(Math.random() * (rangomax - rangomin + 1) + rangomin);
+  let b = parseInt(Math.random() * (rangomax - rangomin + 1) + rangomin);
+
+  AplicarColoresInterfaz(r ,g , b)
+
+  let random;
   random = Math.floor(Math.random() * 2);
-
+  
   return [r, g, b, random];
 }
 
@@ -28,15 +62,15 @@ export const GenerarVariacion = () => {
   console.log(r, g, b);
   switch (random) {
     case 0:
-      r += 9;
-      g += 9;
-      b += 9;
+      r += num;
+      g += num;
+      b += num;
       console.log("Suma");
       break;
     case 1:
-      r -= 9;
-      g -= 9;
-      b -= 9;
+      r -= num;
+      g -= num;
+      b -= num;
       console.log("Resta");
       break;
   }
